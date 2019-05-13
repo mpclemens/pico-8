b91a94efc39a1fcbe6f5bec397d35bf53a3a0966
Mike Clemens <mclemens128@gmail.com>
2019-05-13 16:37:10 -0700

    Removing out-of-bounds balls, tweaking layout

--- pinphys/pinphys.lua
+++ pinphys/pinphys.lua
@@ -7,9 +7,9 @@ function new_g()
     -- render colors and game sizes
     ["bc"] = 0,
     ["wc"] = 1,
-    ["x"] = 0,
+    ["x"] = 10,
     ["y"] = 0,
-    ["w"] = 100,
+    ["w"] = 110,
     ["h"] = 128,
     ["ew"] = 3, -- edge width
     ["lw"] = 8, -- lane width, for inlanes/drains
@@ -24,9 +24,9 @@ end
 
 function init_g(g)
   g.bs = {
-    new_b(20, 10),
-    new_b(20, 20, 1),
-    new_b(20, 15, - 1)
+    new_b(g, 40, 20, rnd(4) - 2, rnd(4) - 2),
+    new_b(g, 50, 20, rnd(4) - 2, rnd(4) - 2),
+    new_b(g, 60, 20, rnd(4) - 2, rnd(4) - 2)
   }
 
   g.lfs = {
@@ -50,22 +50,21 @@ function draw_g(g)
   -- rectfill(g.x + p, g.y + g.h - p, g.x + p + bw, g.y + g.h, g.bc) -- l outlane
 
   -- inlanes is a rect, with a circle cutout and two flippers
-  local ilr = g.dw * 2 -- radius of inline cutout
+  local ilr = g.dw * 2.5 -- radius of inline cutout
   local ily = g.y + g.h - ilr -- inlane starting "y" value
   rectfill(g.x + g.ew + g.lw, ily, g.x + g.w - g.ew - g.lw, g.y + g.h - g.ew, g.wc) -- inlane walls
   circfill(g.x + g.w / 2, ily, ilr, g.bc) -- inlane cutout
 
-  g.lfs[1].x = g.x + g.w / 2 - ilr
+  -- lflip
+  g.lfs[1].x = g.x + g.w / 2 - g.dw * 2
   g.lfs[1].y = g.y + g.h - g.lfs[1].h + g.ew
 
-  g.rfs[1].x = g.x + g.w / 2 + ilr - g.rfs[1].w
+  -- rflip
+  g.rfs[1].x = g.x + g.w / 2 + g.dw * 2 - g.rfs[1].w
   g.rfs[1].y = g.y + g.h - g.rfs[1].h + g.ew
 
-
-  -- spr(17, g.x + g.w / 2 - bw * 4, g.y + g.h - p - bw - 16, 2, 2)-- l slingshot
-  -- rectfill(g.x + g.w - p, g.y + g.h - p, g.x + g.w - p - bw, g.y + g.h, g.bc) -- r outlane
-  -- r slingshot
-
+  spr(17, g.x + g.ew + g.lw, ily - 16, 2, 2)-- l sling
+  spr(19, g.x + g.w - g.ew - g.lw - 16 + 1, ily - 16, 2, 2)-- r sling
 
   for f in all(g.lfs) do
     draw_f(f)
@@ -76,7 +75,11 @@ function draw_g(g)
   end
 
   for b in all(g.bs) do
-    draw_b(b)
+    if b.live then
+      draw_b(b)
+    else
+      del(g.bs, b)
+    end
   end
 end
 
@@ -84,7 +87,7 @@ function update_g(g)
 
   -- update ball positions
   for b in all(g.bs) do
-    move_b(b)
+    if b.live then move_b(b) end
   end
 
   -- reset
@@ -95,7 +98,7 @@ function update_g(g)
   -- nudge controls
   if (btnp(5)) then
     for b in all(g.bs) do
-      nudge_b(b)
+      if b.live then nudge_b(b) end
     end
   end
 
@@ -107,92 +110,84 @@ function update_g(g)
   for f in all(g.rfs) do
     f.up = btn(1)
   end
+
+  -- add-a-ball for xxx testing, down-arrow
+  if (btnp(3)) then
+    if (#g.bs < 5) then
+      sfx(1)
+      add(g.bs, new_b(g, g.x + g.w / 2, g.y + g.w / 2 - g.ew, rnd(4) - 3, rnd(4) - 3))
+    else
+      sfx(2)
+    end
+  end
 end
 
 -- BALLS
 
-function new_b(x, y, vx, vy)
+function new_b(g, x, y, vx, vy)
   return {
-    ["g"] = {}, -- containing game reference
+    ["g"] = g, -- containing game reference
     ["x"] = x,
     ["y"] = y,
     ["w"] = 6, -- actual visible pixels of sprite
     ["h"] = 6,
     ["vx"] = (vx == nil and 0 or vx),
     ["vy"] = (vy == nil and 0 or vy),
-    ["spr"] = 1
+    ["spr"] = 1,
+    ["live"] = true
   }
 end
 
 function draw_b(b)
   spr(b.spr, b.x, b.y)
-  line(b.x + b.w / 2, b.y + b.h / 2, b.x + b.w / 2 + b.vx, b.y + b.h / 2 + b.vy, 10) -- xxx collision line
+  --  line(b.x + b.w / 2, b.y + b.h / 2, b.x + b.w / 2 + b.vx * 5, b.y + b.h / 2 + b.vy * 5, 7) -- xxx move vector
 end
 
+function sgn(n)
+  -- -1 if n < 0, 0 if n == 0, 1 if n > 0
+  if n == 0 then
+    return 0
+  end
+
+  return n / abs(n)
+end
 
 function move_b(b)
+
+  -- out of bounds
+  if b.x < b.g.x or b.x > b.g.x + b.g.w or b.y < b.g.y or b.y > b.g.y + b.g.h then
+    b.live = false
+    return
+  end
 
   local newx = b.x + b.vx
   local newy = b.y + b.vy
 
-  -- look at the middle of the ball at the next piece
-  if pget(b.x + b.w + 1 + b.vx, b.y + b.h + 1 + b.vy) ~= 0 then
-    sfx(1)
-  end
-
-  if b.x + b.vx < 0 + 7 or
-  b.x + b.vx > 128 - 7
-  then
-    -- bounce on side!
-    b.vx = b.vx * - 1
-    sfx(1)
-  else
-    b.x = b.x + b.vx
-  end
-
-  -- move ball up/down
-  if b.y + b.vy < 0 + 7 or b.y + b.vy > 128 - 7
-  then
-    -- bounce on floor/ceiling
-    b.vy = b.vy * - 0.9
-  else
-    b.y = b.y + b.vy
-  end
-
   -- friction!
-  b.vx = b.vx * 0.97
+  b.vx = b.vx * 0.99
   b.vy = b.vy * 0.97
 
   -- gravity!
   b.vy = b.vy + 0.2
-end
 
-function move_simple_b(b)
-  if b.x + b.vx < 0 + 7 or
-  b.x + b.vx > 128 - 7
-  then
-    -- bounce on side!
-    b.vx = b.vx * - 1
-    sfx(1)
-  else
-    b.x = b.x + b.vx
+  -- quantum!
+  b.vx = b.vx + (rnd(10) - 5) / 1000
+
+  -- not moving
+  if abs(b.vx) <= 0.25 and abs(b.vy) <= 0.25 then return end
+
+  -- check the new x and new y for collisions
+  local nx = b.x + b.w + b.vx * sgn(b.vx)
+  local ny = b.y + b.h + b.vy * sgn(b.vy)
+
+  -- xxx better checker here, identify what was hit
+  if pget(nx, ny) ~= b.g.bc then
+    b.vx = 0 - b.vx
+    b.vy = 0 - b.vy
   end
 
-  -- move ball up/down
-  if b.y + b.vy < 0 + 7 or b.y + b.vy > 128 - 7
-  then
-    -- bounce on floor/ceiling
-    b.vy = b.vy * - 0.9
-  else
-    b.y = b.y + b.vy
-  end
-
-  -- friction!
-  b.vx = b.vx * 0.97
-  b.vy = b.vy * 0.97
-
-  -- gravity!
-  b.vy = b.vy + 0.2
+  b.x = b.x + b.vx
+  b.y = b.y + b.vy
 end
 
 function nudge_b(b)
